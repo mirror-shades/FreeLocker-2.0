8d4572f1021aea0911c662dd2a69f80ecf746305
null <null>
2024-10-06 17:13:13 -0700

    link to metamask if uninstalled

--- src/components/Wallet.tsx
+++ src/components/Wallet.tsx
@@ -1,22 +1,46 @@
+import { useEffect, useState } from "react";
 import Faucet from "./Faucet";
 
 const NETWORK_ID = "11155111";
+const METAMASK_DOWNLOAD_URL = "https://metamask.io/download/";
 
 export default function Web3App({ connectWallet, networkCheck, account }: {account:string, networkCheck:string, connectWallet:Function}) {
+   const [isMetaMaskInstalled, setIsMetaMaskInstalled] = useState<boolean | null>(null);
+
+   useEffect(() => {
+     const checkMetaMaskInstallation = () => {
+       setIsMetaMaskInstalled(typeof window.ethereum !== 'undefined');
+     };
+     checkMetaMaskInstallation();
+   }, []);
+
+   let content;
    
-    let content = <div>Finding Metamask...</div>;
-    if (networkCheck === NETWORK_ID && account) {
-        content = (
-            <div>
-                <Faucet account={account}/>
-            </div>
-        );
-    } else {
-        content = (
-            <button className="invisibleButton" onClick={() => {connectWallet()}}>
-                <img className="metamaskButton w-32" src="src/assets/MetaMask_Fox.png" alt="Metamask" />
-            </button>
-        );
-    }
-    return content;
+   if (isMetaMaskInstalled === null) {
+     content = <div>Checking for MetaMask...</div>;
+   } else if (networkCheck === NETWORK_ID && account) {
+     content = (
+       <div>
+         <Faucet account={account}/>
+       </div>
+     );
+   } else {
+     content = (
+       <a 
+         href={isMetaMaskInstalled ? "#" : METAMASK_DOWNLOAD_URL} 
+         target={isMetaMaskInstalled ? "_self" : "_blank"} 
+         rel={isMetaMaskInstalled ? "" : "noopener noreferrer"}
+         onClick={(e) => {
+           if (isMetaMaskInstalled) {
+             e.preventDefault();
+             connectWallet();
+           }
+         }}
+       >
+         <img className="metamaskButton w-32" src="src/assets/MetaMask_Fox.png" alt="Metamask" />
+       </a>
+     );
+   }
+   
+   return content;
 }
